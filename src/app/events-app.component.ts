2819ff3bc3d5ee966133f4506ec1276ddb17df47
David Stevens <jdavid.stevens@gmail.com>
2019-11-27 09:27:45 -0700

    Added navbar

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'events-app',
-  template: `<h2>Hello World</h2>
-  <img src="/assets/images/basic-shield.png">
-  `
+  template: `
+  <nav-bar></nav-bar>
+  <events-list></events-list>`
 })
 export class EventsAppComponent {
   title = 'ng-fundamentals';
