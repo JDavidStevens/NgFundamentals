0c6216d63bb7d6cca35b9a7676e7f7f3cf97ecbc
David Stevens <james.stevens@healthcatalyst.com>
2019-12-14 19:34:48 -0700

    strict type initiated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import {
+  EventsListComponent,
+  EventThumbnailComponent,
+  EventService,
+  EventDetailsComponent,
+  CreateEventComponent,
+  EventRouteActivator,
+  EventListResolver
+} from './events/index';
+
 import { ToastrModule } from 'ngx-toastr';
 
 import { EventsAppComponent } from './events-app.component';
-import { EventsListComponent } from './events/events-list.component';
-import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { NavBarComponent } from './nav/navbar.component';
-import { EventDetailsComponent } from './events/event-details/event-details.component';
-
-import { EventService } from './events/shared/event.service';
 import { appRoutes } from './routes';
-import { CreateEventComponent } from './events/shared/create-event.component';
 import { Error404Component } from './errors/404.component';
-import { EventRouteActivator } from './events/event-details/event-route-activator.service';
-import { EventListResolver } from './events/shared/event-list-resolver.service';
-
 @NgModule({
   declarations: [
     EventsAppComponent,
@@ -26,7 +27,7 @@ import { EventListResolver } from './events/shared/event-list-resolver.service';
     NavBarComponent,
     EventDetailsComponent,
     CreateEventComponent,
-    Error404Component
+    Error404Component,
   ],
   imports: [
     BrowserModule,
