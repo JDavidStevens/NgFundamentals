ecf17a80014d73f629110214893d72b6ea7a76f4
David Stevens <james.stevens@healthcatalyst.com>
2020-01-23 09:04:52 -0700

    added arrays and select

--- src/app/events/experiment/experiment.component.ts
+++ src/app/events/experiment/experiment.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExperimentComponent implements OnInit {
 
+  public manufacturer: Array<string> = ['Honda', 'Husqvarna', 'Kawasaki', 'KTM', 'Suzuki', 'Yamaha'];
+
+  public honda: Array<string> = ['CRF/R', 'CRF/X'];
+  public husqvarna: Array<string> = ['FE', 'TE'];
+  public kawasaki: Array<string> = ['KXF', 'KLX'];
+  public ktm: Array<string> = ['XC', 'SX'];
+  public suzuki: Array<string> = ['RMZ', 'RMX'];
+  public yamaha: Array<string> = ['YZ', 'WR'];
+
   constructor() { }
 
   ngOnInit() {
